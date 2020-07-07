316c1182d620b9a56e4cd7faa914363753794803
kwnlee <kwnylee@gmail.com>
2020-07-07 13:09:31 +0900

    0707kw

--- src/main/java/com/jujumarket/order/service/ItemService.java
+++ src/main/java/com/jujumarket/order/service/ItemService.java
@@ -1,20 +1,20 @@
-package com.jujumarket.order.service;
-
-import java.util.List;
-
-import com.jujumarket.order.domain.ItemVO;
-
-public interface ItemService {
-	
-	public void register(ItemVO item);
-	
-	public ItemVO get(String itemCode);
-	
-	public boolean modify(ItemVO item);
-	
-	public boolean remove(String itemCode);
-	
-	public List<ItemVO> getList();
-	
-
-}
+//package com.jujumarket.order.service;
+//
+//import java.util.List;
+//
+//import com.jujumarket.order.domain.ItemVO;
+//
+//public interface ItemService {
+//	
+//	public void register(ItemVO item);
+//	
+//	public ItemVO get(String itemCode);
+//	
+//	public boolean modify(ItemVO item);
+//	
+//	public boolean remove(String itemCode);
+//	
+//	public List<ItemVO> getList();
+//	
+//
+//}
