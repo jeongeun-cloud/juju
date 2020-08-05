e0bc9a1615d250f7060941bf5039829ef02801ce
cse <whtjd999@naver.com>
2020-08-05 10:56:26 +0900

    땡겨 오기 위한 by se

---
+++ src/main/java/com/jujumarket/admin/mapper/QnaListMapper.java
@@ -0,0 +1,12 @@
+package com.jujumarket.admin.mapper;
+
+import java.util.List;
+
+import com.jujumarket.admin.domain.QnaVO;
+
+public interface QnaListMapper {
+	
+	public List<QnaVO> getQnaList();
+
+
+}
