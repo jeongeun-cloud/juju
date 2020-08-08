1a7d0569c7b90df11499394754e86cd81089ec66
kwnlee <kwnylee@gmail.com>
2020-08-08 10:17:12 +0900

    BoardController.java

--- src/main/java/com/jujumarket/board/controller/BoardQNAController.java
+++ src/main/java/com/jujumarket/board/controller/BoardQNAController.java
@@ -27,95 +27,95 @@ import lombok.extern.log4j.Log4j;
 
 public class BoardQNAController {
 
-   private BoardQNAService service;
+	private BoardQNAService service;
 
-   /*
-    * @GetMapping("/myQna/list") public void list(Model model) {
-    * 
-    * log.info("list"); model.addAttribute("list", service.getList());
-    * 
-    * }
-    */
+	/*
+	 * @GetMapping("/myQna/list") public void list(Model model) {
+	 * 
+	 * log.info("list"); model.addAttribute("list", service.getList());
+	 * 
+	 * }
+	 */
 
-   @PostMapping("/myQna/register")
-   public String register(BoardQNAVO qna, RedirectAttributes rttr) {
-      log.info("register : " + qna);
+	@PostMapping("/myQna/register")
+	public String register(BoardQNAVO qna, RedirectAttributes rttr) {
+		log.info("register : " + qna);
 
-      service.register(qna);
+		service.register(qna);
 
-      rttr.addFlashAttribute("result", qna.getPostingNo());
+		rttr.addFlashAttribute("result", qna.getPostingNo());
 
-      return "redirect:/mypage/myQna/list";
+		return "redirect:/mypage/myQna/list";
 
-   }
+	}
 
-   @GetMapping("/myQna/list")
-   public String list(Criteria cri, Model model, HttpSession session) {
-      MemberVO member = (MemberVO) session.getAttribute("sessionMember");
-      if (member == null) {
-         return "redirect:/member/login";
+	@GetMapping("/myQna/list")
+	public String list(Criteria cri, Model model, HttpSession session) {
+		MemberVO member = (MemberVO) session.getAttribute("sessionMember");
+		if (member == null) {
+			return "redirect:/member/login";
 
-      }
-      String idNo = member.getIdNo();
+		}
+		String idNo = member.getIdNo();
 
-      log.info("qna" + cri);
-      model.addAttribute("qna", service.getListByIdNo(idNo));
-      int total = service.getTotal(cri);
-      if (cri.getKeyword() != null && cri.getKeyword() != "") {
-         int resultTotal = service.getResultTotalByIdNo(cri, idNo);
-         System.out.println("resultTotal" + resultTotal);
-         model.addAttribute("pageMaker", new PageDTO(cri, resultTotal));
-      } else {
-         System.out.println("total" + total);
-         model.addAttribute("pageMaker", new PageDTO(cri, total));
-      }
+		log.info("qna" + cri);
+		model.addAttribute("qna", service.getListByIdNo(idNo));
+		int total = service.getTotal(cri);
+		if (cri.getKeyword() != null && cri.getKeyword() != "") {
+			int resultTotal = service.getResultTotalByIdNo(cri, idNo);
+			System.out.println("resultTotal" + resultTotal);
+			model.addAttribute("pageMaker", new PageDTO(cri, resultTotal));
+		} else {
+			System.out.println("total" + total);
+			model.addAttribute("pageMaker", new PageDTO(cri, total));
+		}
 
-      return "/mypage/myQna/list";
-   }
+		return "/mypage/myQna/list";
+	}
 
-   @GetMapping("/myQna/register")
-   public void register() {
+	@GetMapping("/myQna/register")
+	public void register() {
 
-   }
-
+	}
 
 	@GetMapping({ "/myQna/get", "/myQna/modify" })
 	public String get(@RequestParam("postingNo") String postingNo, @ModelAttribute("cri") Criteria cri, Model model,
 			HttpSession session, RedirectAttributes rttr) {
 		MemberVO member = (MemberVO) session.getAttribute("sessionMember");
 
-      if (member == null) {
-         rttr.addFlashAttribute("result", "로그인 후 이용 가능합니다.");
-         return "redirect:/mypage/myQna/list";
-      } else if ((member.getMemCode().equals("ADMIN"))||(member.getIdNo().equals(service.getIdNoByPostingNo(postingNo)))) {
-    	  model.addAttribute("BoardQNA", service.get(postingNo));
-    	  model.addAttribute("qna", service.get(postingNo));
-         return "/mypage/myQna/get";
-      } else{
-         return "/mypage/myQna/list";
-      }
-   }
+		if (member == null) {
+			rttr.addFlashAttribute("result", "로그인 후 이용 가능합니다.");
+			return "redirect:/mypage/myQna/list";
+		} else if ((member.getMemCode().equals("ADMIN"))
+				|| (member.getIdNo().equals(service.getIdNoByPostingNo(postingNo)))) {
+			model.addAttribute("BoardQNA", service.get(postingNo));
+			model.addAttribute("qna", service.get(postingNo));
+			return "/mypage/myQna/get";
+		} else {
+			return "/mypage/myQna/list";
+		}
+	}
 
-   @PostMapping("/myQna/modify")
-   public String modify(BoardQNAVO qna, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
-      log.info("modify : " + qna);
+	@PostMapping("/myQna/modify")
+	public String modify(BoardQNAVO qna, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
+		log.info("modify : " + qna);
 
-      if (service.modify(qna)) {
-         rttr.addFlashAttribute("result", "success");
-      }
+		if (service.modify(qna)) {
+			rttr.addFlashAttribute("result", "success");
+		}
 
-      return "redirect:/mypage/myQna/list" + cri.getListLink();
-   }
+		return "redirect:/mypage/myQna/list" + cri.getListLink();
+	}
 
-   @PostMapping("/myQna/remove")
-   public String remove(@RequestParam("postingNo") String postingNo, @ModelAttribute("cri") Criteria cri,
-         RedirectAttributes rttr) {
+	@PostMapping("/myQna/remove")
+	public String remove(@RequestParam("postingNo") String postingNo, @ModelAttribute("cri") Criteria cri,
+			RedirectAttributes rttr) {
 
-      log.info("remove....." + postingNo);
-      if (service.remove(postingNo)) {
-         rttr.addFlashAttribute("result", "success");
-      }
-      return "redirect:/mypage/myQna/list" + cri.getListLink();
-   }
+		log.info("remove....." + postingNo);
+		if (service.remove(postingNo)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/mypage/myQna/list" + cri.getListLink();
+	}
 
 }
